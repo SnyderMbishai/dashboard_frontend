c51849cfe5a4c5b23275321d56d63fa13b805514
SnyderMbishai <csmbishai@gmail.com>
2020-07-06 18:24:11 +0300

    Display the graphs as cards

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
     <div className="App">  
       <h1>PBP Production Dashboard</h1>
       <div className="grid-container">
-        <BottomFiveByPending/>
-        <FilteredByDateRange/>
-        </div>
+        <div className="container card"><BottomFiveByPending/></div>
+        <div className="container card"><FilteredByDateRange/></div>          
+      </div>
       <div className="grid-container">
-        <TopTenByProduced/>
-        <TopFiveByRejected/>
+        <div className="container card"><TopTenByProduced/></div>
+        <div className="container card"><TopFiveByRejected/></div>    
       </div>      
     </div>
   );
